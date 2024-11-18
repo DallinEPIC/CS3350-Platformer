20e53e0c88fea2a8f8ff4573c2c078104c9ddfe7
Thomas Drown <72996582+tomatom92@users.noreply.github.com>
2024-11-18 12:05:28 -0700

    made level 2, fixed spawn point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public static PlayerController instance;
     [SerializeField] private TextMeshProUGUI _scoreUIText;
     [HideInInspector] public int fruitCollected;
+    private Transform spawnPoint;
+    
     void Start()
     {
         instance = this;
         fruitCollected = 0;
         DontDestroyOnLoad(this);
+        spawnPoint = SceneController.instance._spawnPoint;
+        spawnPoint.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,5 +28,7 @@ public class PlayerController : MonoBehaviour
     public void Die()
     {
         Debug.Log("Player died");
+        transform.position = spawnPoint.position;
     }
+
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static SceneController instance;
+    [SerializeField] public Transform _spawnPoint;
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
